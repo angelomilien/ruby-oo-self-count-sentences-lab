32c50a5ed34dfa7a679db3316ff6109ae6cb72a1
angelo milien <angelomilienjoseph@gmail.com>
2020-10-21 08:06:54 -0400

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
+    !!self.end_with?(".")
     
   end
 
   def question?
-
+    !!self.end_with?("?")
   end
 
   def exclamation?
-
+    !!self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.!?]/)
+    new_value = array.reject { |e| e.to_s.empty? }
+    new_value.length
   end
+ 
 end
